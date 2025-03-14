56586448305e9fc7683fc46b8aed15631cd3d0ba
TengLi <18547241+TenTen-Teng@users.noreply.github.com>
2025-03-13 20:15:49 -0700

    unit test constant.R

--- tests/testthat/test-constant.R
+++ tests/testthat/test-constant.R
@@ -1,1 +1,12 @@
 # constant.R unit tests
+
+test_that(
+  "test constant variables", {
+    expect_equal(base_url, "https://api.weatherbit.io/v2.0/")
+    expect_is(api_key, "character")
+    expect_is(valid_langs, "character")
+    expect_in("en", valid_langs)
+    expect_is(vaild_units, "character")
+    expect_in("M", vaild_units)
+  }
+)
