ffbef7c84c43acc224075d37762680a736506961
TengLi <18547241+TenTen-Teng@users.noreply.github.com>
2025-03-16 21:55:26 -0700

    Update import lib

--- R/forecast.R
+++ R/forecast.R
@@ -22,7 +22,9 @@ forecast_url <- paste(base_url, endpoint, sep='')
 #' @param language Language, default as English
 #' @param unit Available units, default as Metric (Celsius, m/s, mm)
 #' @param day Forecast days, default as 16
+#' @importFrom magrittr %>%
 #' @importFrom httr GET
+#' @export
 get_forecast_by_city <- function(
   city, save_dir = '', language = 'en', unit = 'M', day = 16
   ){
@@ -67,7 +69,7 @@ get_forecast_by_city <- function(
   params$day <- day
 
   # Connect API.
-  response <- httr::GET(forecast_url, query = params)
+  response <- GET(forecast_url, query = params)
 
   if (response$status_code != 200){
     # Extract content as text
@@ -116,7 +118,9 @@ get_forecast_by_city <- function(
 #' @param language Language, default as English
 #' @param unit Available units, default as Metric (Celsius, m/s, mm)
 #' @param day Forecast days, default as 16
+#' @importFrom magrittr %>%
 #' @importFrom httr GET
+#' @export
 get_forecast_by_lat_lon <- function(
   lat, lon, save_dir = '', language = 'en', unit = 'M', day = 16
   ){
@@ -218,6 +222,9 @@ get_forecast_by_lat_lon <- function(
 #' @param language Language, default as English
 #' @param unit Available units, default as Metric (Celsius, m/s, mm)
 #' @param day Forecast days, default as 16
+#' @importFrom magrittr %>%
+#' @importFrom httr GET
+#' @export
 get_forecast_by_postal_code <- function(
   postal_code, country, save_dir = '', language = 'en', unit = 'M', day = 16
   ){
@@ -313,6 +320,9 @@ get_forecast_by_postal_code <- function(
 #' @param language Language, default as English
 #' @param unit Available units, default as Metric (Celsius, m/s, mm)
 #' @param day Forecast days, default as 16
+#' @importFrom magrittr %>%
+#' @importFrom httr GET
+#' @export
 get_forecast_by_city_id <- function(
   city_id, save_dir = '', language = 'en', unit = 'M', day = 16
   ){
